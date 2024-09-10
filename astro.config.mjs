8bf147838b74be8c3875faf729d1496f1b522f2a
Magdalena  Martinez <magdalenamartinez@magdalenas-mbp.lan>
2024-09-09 17:16:18 -0700

    manual rollback

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
 	site: "https://blahg.netlify.app/",
 	base: "/",
-	adapter: vercel({
-		webAnalytics: { enabled: true }
-	  }),
 	integrations: [sitemap()],
 	markdown: {
 		shikiConfig: {
