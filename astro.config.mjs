3a9f18afeb860e2859902ac9e0f1a6ca02be588e
Magdalena  Martinez <magdalenamartinez@magdalenas-mbp.lan>
2024-09-09 17:05:31 -0700

    analytics added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
+import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
 	site: "https://blahg.netlify.app/",
 	base: "/",
+	adapter: vercel({
+		webAnalytics: { enabled: true }
+	  }),
 	integrations: [sitemap()],
 	markdown: {
 		shikiConfig: {
